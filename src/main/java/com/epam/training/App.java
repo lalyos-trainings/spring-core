0deb385ef9414e3a8d9ba3ea99735ec639bf3fba
lalyos <lalyos@yahoo.com>
2013-02-11 14:49:33 +0100

    ApplicationContext instead of BeanFactory

--- src/main/java/com/epam/training/App.java
+++ src/main/java/com/epam/training/App.java
@@ -2,6 +2,8 @@ package com.epam.training;
 
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.FileSystemResource;
 
@@ -13,10 +15,9 @@ public class App
 {
     public static void main( String[] args )
     {
-        FileSystemResource resource = new FileSystemResource("src/main/resources/beans.xml");
-        ClassPathResource cpresource = new ClassPathResource("beans.xml");
-        BeanFactory factory = new XmlBeanFactory(cpresource);
-        GreetingApp app1 = factory.getBean("multiGreeting",GreetingApp.class);
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("beans.xml"); 
+        
+        GreetingApp app1 = ctx.getBean("multiGreeting",GreetingApp.class);
         app1.greet();
         
     }
