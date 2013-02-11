7f2bb693d6f8248b868e019d91818e1792576018
lalyos <lalyos@yahoo.com>
2013-02-11 14:44:20 +0100

    switch to ClassPtahResource

--- src/main/java/com/epam/training/App.java
+++ src/main/java/com/epam/training/App.java
@@ -2,6 +2,7 @@ package com.epam.training;
 
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.FileSystemResource;
 
 /**
@@ -13,7 +14,8 @@ public class App
     public static void main( String[] args )
     {
         FileSystemResource resource = new FileSystemResource("src/main/resources/beans.xml");
-        BeanFactory factory = new XmlBeanFactory(resource);
+        ClassPathResource cpresource = new ClassPathResource("beans.xml");
+        BeanFactory factory = new XmlBeanFactory(cpresource);
         GreetingApp app1 = factory.getBean("multiGreeting",GreetingApp.class);
         app1.greet();
         
